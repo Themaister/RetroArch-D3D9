02c48949c74c8a649c89c4e312235bcba8f1f353
Themaister <maister@archlinux.us>
2011-11-27 02:33:31 +0100

    Minor fixes.

--- D3DVideo.h
+++ D3DVideo.h
@@ -32,15 +32,6 @@ class D3DVideo
 
    private:
 
-      struct
-      {
-         bool active;
-         HMODULE lib;
-         HRESULT (*WINAPI dwm_flush)();
-      } dwm;
-      void init_dwm();
-      void deinit_dwm();
-
       WNDCLASSEX windowClass;
       HWND hWnd;
       IDirect3D9 *g_pD3D;
@@ -62,7 +53,6 @@ class D3DVideo
       ssnes_video_info_t video_info;
 
       bool needs_restore;
-      bool dwm_enabled;
       bool restore();
 
       CGcontext cgCtx;

396835750078fdae74d01494a18d9496390aed32
Themaister <maister@archlinux.us>
2011-12-03 16:31:52 +0100

    Fix Cg being a bit stupid :(

--- render_chain.cpp
+++ render_chain.cpp
@@ -810,19 +810,58 @@ void RenderChain::unbind_all()
    bound_vert.clear();
 }
 
-static inline CGparameter find_param_from_semantic(CGprogram prog, const std::string &sem)
+static inline bool validate_param_name(const std::string &name)
 {
-   CGparameter param = cgGetFirstParameter(prog, CG_PROGRAM);
+   static const char *illegal[] = {
+      "PREV.",
+      "PREV1.",
+      "PREV2.",
+      "PREV3.",
+      "PREV4.",
+      "PREV5.",
+      "PREV6.",
+      "ORIG.",
+      "IN.",
+      "PASS",
+   };
+
+   for (unsigned i = 0; i < sizeof(illegal) / sizeof(illegal[0]); i++)
+      if (name.find_first_of(illegal[i]) == 0)
+         return false;
+
+   return true;
+}
+
+static inline CGparameter find_param_from_semantic(CGparameter param, const std::string &sem)
+{
    while (param)
    {
-      if (cgGetParameterSemantic(param) &&
-            sem == cgGetParameterSemantic(param) &&
-            cgGetParameterDirection(param) == CG_IN)
-         return param;
+      if (cgGetParameterType(param) == CG_STRUCT)
+      {
+         CGparameter ret = find_param_from_semantic(cgGetFirstStructParameter(param), sem);
+         if (ret)
+            return ret;
+      }
+      else
+      {
+         if (cgGetParameterSemantic(param) &&
+               sem == cgGetParameterSemantic(param) &&
+               cgGetParameterDirection(param) == CG_IN &&
+               cgGetParameterVariability(param) == CG_VARYING && validate_param_name(cgGetParameterName(param)))
+         {
+            std::cerr << "Found semantic: " << sem << " for param: " << cgGetParameterName(param) << std::endl;
+            return param;
+         }
+      }
       param = cgGetNextParameter(param);
    }
 
    return nullptr;
+}
+
+static inline CGparameter find_param_from_semantic(CGprogram prog, const std::string &sem)
+{
+   return find_param_from_semantic(cgGetFirstParameter(prog, CG_PROGRAM), sem);
 }
 
 #define DECL_FVF_POSITION(stream) \
@@ -871,8 +910,8 @@ void RenderChain::init_fvf(Pass &pass)
       param = find_param_from_semantic(pass.vPrg, "POSITION0");
    if (param)
    {
-      std::cerr << "[FVF]: POSITION semantic found!" << std::endl;
       first_indice = 1;
+      std::cerr << "[FVF]: POSITION semantic found!" << std::endl;
       unsigned index = cgGetParameterResourceIndex(param);
       decl[index] = position_decl;
       indices[index] = true;
